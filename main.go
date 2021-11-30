73d8d7b840ebe3be5fb5505b14ba644af8a96382
berlink <2285525802@qq.com>
2021-11-30 10:50:37 +0800

    read version server finish

--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main(){
 
+	db, err := readFile("student set.json")
+	if err != nil{
+		fmt.Println("db init fail")
+		return
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/show", http.HandlerFunc(db.server_show_list))
+	mux.Handle("/order", http.HandlerFunc(db.price))
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 
+}
 
+func (db stulist) server_show_list(w http.ResponseWriter, req *http.Request) {
+	for index, stu := range db {
+		fmt.Fprintf(w, "%s: %s\n", index, stu)
+	}
+}
 
+func (db stulist) server_show_order(w http.ResponseWriter, req *http.Request) {
+	db = db.order()
+	for index, stu := range db {
+		fmt.Fprintf(w, "%s: %s\n", index, stu)
+	}
 }
 
 
